89e5264cc49301396706cbd0acba1359f0711f6b
Cloudymolecule <baires82@gmail.com>
2021-02-24 18:19:48 -0500

    commit tree 747b010f1b1a91cf042706b5fa8d3163c2821bf2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function superbowlWin(array) {
+    let element = array.find(function(e){
+        if (e.result === 'W'){
+            return e
+        }
+    })
+    if (element) {
+        return element.year
+    } 
+}
+
